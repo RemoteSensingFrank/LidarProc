cd3ea5b4e7fb727a1fcd4bfb4b72830e19d2b5fe
RemoteSensingFrank <wuwei_cug@163.com>
2019-04-21 14:19:02 +0800

    添加cpp-httplib服务器，部署potree前端点云数据展示平台，添加potree源码，需要进步一根据我的点云读写方式修改源码移除对liblas的依赖

---
+++ src/LidarServer.cpp
@@ -0,0 +1,22 @@
+// LidarProc.cpp: 定义控制台应用程序的入口点。
+//
+#include<stdio.h>
+#include "./LidarServer/httplib.h"
+
+int main(void)
+{
+    using namespace httplib;
+
+    Server svr;
+
+    svr.Get("/hi", [](const Request& req, Response& res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
+        auto numbers = req.matches[1];
+        res.set_content(numbers, "text/plain");
+    });
+    svr.set_base_dir("./www");
+    svr.listen("localhost", 1234);
+}
---
+++ src/LidarServer/LidarServerProcGet.h
@@ -0,0 +1,7 @@
+/**
+desc:the lidar proc server get the command and 
+process the data and then share the data into
+the share directory and others can using the
+js code to view the process result
+author: Frank.Wu
+**/
