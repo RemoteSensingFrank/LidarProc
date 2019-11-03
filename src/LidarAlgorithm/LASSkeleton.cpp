f267e80c115f26e7dd467b746b173aa691328168
RemoteSensingFrank <wuwei_cug@63.com>
2019-11-03 15:06:59 +0800

    骨架提取算法
    完成简单的根据重心点提取的算法，不考虑噪声点的影响

--- src/LidarAlgorithm/LASSkeleton.cpp
+++ src/LidarAlgorithm/LASSkeleton.cpp
@@ -1,13 +1,108 @@
 #include "LASSeleton.h"
+#include <set>
+
 #include "../LidarBase/LASPoint.h"
 #include "../LidarBase/LASReader.h"
 
 #include"GeometryAlgorithm.h"
 #include"GeometryFlann.h"
 
-#include<gdal_priv.h>
 using namespace GeometryLas;
+using namespace std;
 namespace LasAlgorithm{
+
+    int PointCloudShrinkSkeleton::PointCloudShrinkSkeleton_Centroid(Point3D pointSet int* clusterIdx,int clusterNum)
+    {
+        //判断重心的标准为点到点簇中所有点的距离之和最小
+        double disMin=999999999;
+        int idxMin=0;
+        
+        for(int i=0;i<clusterNum;++i)
+        {
+            double disSigma=0;
+            for(int j=0;j<clusterNum;++j)
+            {
+                disSigma+=DistanceComputation::Distance(pointSet[clusterIdx[i]],pointSet[clusterIdx[j]]);
+            }
+            disMin=disSigma<disMin?disSigma:disMin;
+            idxMin=disSigma<disMin?clusterIdx[i]:clusterIdx[idxMin];
+        }
+        return idxMin
+    }
+
+    Point3Ds PointCloudShrinkSkeleton::PointCloudShrinkSkeleton_Once(Point3Ds pointSet,int nearPointNum)
+    {
+        //构建kdtree
+        typedef PointCloudAdaptor<std::vector<Point3D>> PCAdaptor;
+		const PCAdaptor pcAdaptorPnts(pointSet);
+
+		typedef KDTreeSingleIndexAdaptor<L2_Simple_Adaptor<double, PCAdaptor>, PCAdaptor, 3> kd_tree;
+		kd_tree treeIndex(3, pcAdaptorPnts, KDTreeSingleIndexAdaptorParams(10));
+		treeIndex.buildIndex();
+        Point3Ds pntSkeSet;
+        
+        size_t *ret_index=new size_t[nearPointNum];
+        double *out_dist_sqrt = new double[nearPointNum];
+        
+        set<int> idxSet;
+        
+        for(int i=0;i<pointSet.size();++i)
+        {
+            double pnt[3]={pointSet[i].x,pointSet[i].y,pointSet[i].z};
+            KNNResultSet<double> resultSet(nearPointNum);
+            treeIndex.findNeighbors(resultSet,&pnt[0],searchParams());
+            bool ifFind = false;
+            for(int i=0;i<nearPointNum;++i)
+            {
+                ifFind = idxSet.find(ret_index[i]==idxSet.end()?true:false;
+            } 
+            if(!ifFind)
+            {
+                int idxCentId = PointCloudShrinkSkeleton_Centroid(pointSet,ret_index,nearPointNum);
+                set<int>::iterator resultIdx = find(idxSet.begin(),idxSet.end(),idxCentid);
+                pntSkeSet.push_back(pointSet[idxCentId]);
+                idxSet.insert(idxCentId);
+            }
+        }
+        delete[]ret_index;ret_index=nullptr;
+        delete[]out_dist_sqrt;out_dist_sqrt=nullptr;
+        idxSet.clear();
+        
+        return pntSkeSet;        
+    }
+
+    
+    Point3Ds  PointCloudShrinkSkeleton::PointCloudShrinkSkeleton_Shrink(Point3Ds pointSet,int nearPointNum,int iteratorNum)
+    {
+        Point3Ds pnts(pointSet);
+        Point3Ds pntTmp;
+        for(int i=0;i<iteratorNum;++i)
+        {
+            pntTmp=PointCloudShrinkSkeleton_Once(pnts,nearPointNum);
+            pnts.clear();
+            pnts=pntTmp;
+            pntTmp.clear();
+        }
+    
+        return pnts;
+    }    
+    
+    
+
+
+    Point3Ds  PointCloudShrinkSkeleton::PointCloudShrinkSkeleton_Shrink(ILASDataset* lasDataset,int nearPointNum,int iteratorNum)
+    {
+        Point3Ds pointSet;
+		for (int i = 0; i < dataset->m_totalReadLasNumber; ++i)
+		{
+			const LASIndex &idx = dataset->m_LASPointID[i];
+			pointSet.push_back(dataset->m_lasRectangles[idx.rectangle_idx].m_lasPoints[idx.point_idx_inRect].m_vec3d);
+		}
+		return PointCloudShrinkSkeleton_Shrink(pointSet,nearPointNum,iteratorNum);   
+    }
+
+
+
 
 
 
--- src/LidarAlgorithm/LASSkeleton.h
+++ src/LidarAlgorithm/LASSkeleton.h
@@ -2,6 +2,9 @@
 //
 // Created by Frank.Wu on 19-10-17.
 // 点云数据骨架提取算法：
+// 1. 简单的骨架提取算法，在不考虑噪声点的情况下提取骨架的方法：
+// 首先根据某一点获取其N近邻个点，在获取这些点的基础上计算N近邻点
+// 的中心点（通过重心法计算中心点），然后迭代，找出所有点
 // 
 // 
 
@@ -30,16 +33,30 @@ namespace LasAlgorithm
         * @param  clusterNum: 聚类簇的点云数目
         * @retval 
         */
-        int PointCloudShrinkSkeleton_Centroid(int* clusterIdx,int clusterNum);
+        int PointCloudShrinkSkeleton_Centroid(Point3Ds pointSet int* clusterIdx,int clusterNum);
 
+        /**
+        * @brief  迭代收缩算法运行一次之后的结果
+        * @note   
+        * @param  pointSet: 点云数据集
+        * @param  nearPointNum: 查询临近点数目
+        * @retval 返回运行一次之后的骨架点点云数据集
+        **/
+        Point3Ds PointCloudShrinkSkeleton_Once(Point3Ds pointSet,int nearPointNum);
+        
+        
         /**
         * @brief  迭代收缩算法
         * @note   
         * @param  pointSet: 点云数据集
         * @param  nearPointNum: 查询临近点数目
         * @param  iteratorNum: 收缩算法迭代次数
-        * @retval 
+        * @retval 返回得到的骨架点点集 
         */
-        long  PointCloudShrinkSkeleton_Shrink(Point3Ds pointSet,int nearPointNum,int iteratorNum);
+        Point3Ds  PointCloudShrinkSkeleton_Shrink(Point3Ds pointSet,int nearPointNum,int iteratorNum);
+        
+        Point3Ds  PointCloudShrinkSkeleton_Shrink(ILASDataset* lasDataset,int nearPointNum,int iteratorNum);
+        
+        
     };
 }
